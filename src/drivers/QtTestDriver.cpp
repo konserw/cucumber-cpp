13c2a834241235144cb899487992bedc952250a6
Kamil Strzempowicz <konserw@gmail.com>
2016-12-29 20:57:04 +0100

    Use pipe to get output from QtTest also on Windows, move pipes - related code to separate header

--- src/drivers/QtTestDriver.cpp
+++ src/drivers/QtTestDriver.cpp
@@ -8,6 +8,7 @@
 #include <QObject>
 #include <QFile>
 #include <cstdio>
+#include "cucumber-cpp/internal/utils/qtCapture.hpp"
 
 namespace cucumber {
 namespace internal {
@@ -27,52 +28,18 @@ private slots:
     }
 };
 
-
 const InvokeResult QtTestStep::invokeStepBody() {
-    QtTestObject test(this);
+    QtTestObject testObject(this);
 
-#if defined(_POSIX_VERSION)
-    const int maxLength = 4096;
-    char buffer[maxLength+1] = {0};
-    int out_pipe[2];
-    int saved_stdout;
-
-    saved_stdout = dup(STDOUT_FILENO);  /* save stdout */
-    if( pipe(out_pipe) != 0 ) {          /* make a pipe */
-        exit(1);
-    }
-    dup2(out_pipe[1], STDOUT_FILENO);   /* redirect stdout to the pipe */
-    close(out_pipe[1]);
-
-    int returnValue = QTest::qExec(&test, 0, NULL);
-
-    dup2(saved_stdout, STDOUT_FILENO);  /* reconnect stdout */
+    qtCapture::Init();
+    qtCapture::BeginCapture();
+    int returnValue = QTest::qExec(&testObject, 0, NULL);
+    qtCapture::EndCapture();
 
     if(returnValue == 0)
         return InvokeResult::success();
     else
-    {
-        read(out_pipe[0], buffer, maxLength); /* read from pipe into buffer */
-        return InvokeResult::failure(buffer);
-    }
-#else
-    QTemporaryFile file;
-    QString fileName;
-    if (file.open()) {
-        fileName = file.fileName();
-    }
-    file.close();
-
-    int returnValue = QTest::qExec(&test, QStringList() << "test" << "-o" << fileName);
-    if(returnValue == 0)
-        return InvokeResult::success();
-    else
-    {
-        file.open();
-        QTextStream ts(&file);
-        return InvokeResult::failure(ts.readAll().toLatin1());
-    }
-#endif
+        return InvokeResult::failure(qtCapture::GetCapture());
 }
 
 }
